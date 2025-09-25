d1276f11043f9c337bcae698dba80ace976d7c28
matfernandez27 <fernandez.mat.g@gmail.com>
2025-09-25 18:53:51 -0300

    añado config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+
+// URL del proyecto Supabase
+window.supabaseUrl = 'https://railkoprsjevdyrxalfu.supabase.co';
+
+// Clave pública (Anon Key)
+window.supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhaWxrb3Byc2pldmR5cnhhbGZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3NjU2ODEsImV4cCI6MjA3NDM0MTY4MX0.cXlcz00HmgBAzsTmqQl7lKZPWR9f3STsLFBFBTiucR0';
+
+// Este archivo es fundamental para que el script pueda inicializar la conexión con Supabase.

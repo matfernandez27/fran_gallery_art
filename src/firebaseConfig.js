b96eaa38cd032477bb519d8526a43980218e1f33
matfernandez27 <fernandez.mat.g@gmail.com>
2026-02-23 08:18:18 -0300

    actualizo api

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,21 +1,24 @@
+// firebaseConfig.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-// Pegá acá tu objeto de configuración de la Firebase Console
+// Reemplaza los valores de abajo con los datos reales de tu consola de Firebase
 const firebaseConfig = {
-  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyB5jPmfbrY0U6bEX_Wp5JUVO7Cgn1udWpA",
+  authDomain: "franfafgallery.firebaseapp.com",
+  projectId: "franfafgallery",
+  storageBucket: "franfafgallery.firebasestorage.app",
+  messagingSenderId: "169862029969",
+  appId: "1:169862029969:web:f9ac670bcf77e09d53d530",
+  measurementId: "G-7EJY7X2T16"
 };
+
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exportar instancias
+// Exportar instancias para usar en main.js y admin.js
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
